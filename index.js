8df3db3b28fac0ecfa8a55fa01013b3ad267d72f
Stephen Saciolo <ssaciolo@gmail.com>
2019-05-01 18:26:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown(20,"Mother's day"){
+function holidayCountdown(days, holiday){
   `It's ${days} days until ${holiday}!`
 }
